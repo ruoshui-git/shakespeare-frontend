2aec3fa0ecb1334be63584c36efaf0e0e58c4bb9
ruoshui-git <ruoshuim@gmail.com>
2020-12-03 16:21:53 -0500

    feat: save to localStorage on update

--- src/modelHistory.ts
+++ src/modelHistory.ts
@@ -1,27 +1,48 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 export default class ModelHistory {
     static total: number = 0;
 
     readonly id: number;
     prompt: string;
-    response: string;
+    output: string;
 
-    constructor(prompt: string, response: string) {
-        this.id = (ModelHistory.total)++;
+    constructor(prompt: string, output: string) {
+        this.id = ModelHistory.total++;
         this.prompt = prompt;
-        this.response = response;
+        this.output = output;
     }
 }
-
+const storageKey = "modelHistory";
 function createHistory() {
-    const { subscribe, set, update } = writable<ModelHistory[]>([]);
+    let initial: ModelHistory[] = [];
+    const stored = localStorage.getItem(storageKey);
+    if (stored) {
+        initial = JSON.parse(stored);
+        ModelHistory.total = initial[0].id + 1;
+
+    }
+
+    const { subscribe, set, update } = writable<ModelHistory[]>(initial);
 
     return {
         subscribe,
-        add: (entry: ModelHistory) => update(history => [entry, ...history]),
-        reset: () => { set([]); ModelHistory.total = 0; },
-    }
+        add: (entry: ModelHistory) =>
+            update((history) => {
+                const newHistory = [entry, ...history];
+                try {
+                    localStorage.setItem(storageKey, JSON.stringify(newHistory));
+                } catch (error) {
+
+                }
+                return newHistory;
+            }),
+        reset: () => {
+            set([]);
+            ModelHistory.total = 0;
+            localStorage.removeItem(storageKey);
+        },
+    };
 }
 
 export const modelHistory = createHistory();
