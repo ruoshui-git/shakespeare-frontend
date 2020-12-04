3b3f7153c619818aa339c7bb64c4ef564b11cece
ruoshui-git <ruoshuim@gmail.com>
2020-12-04 00:25:41 -0500

    feat: set up routing

---
+++ src/Index.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Header from "./components/Header.svelte";
+
+  import About from "./About.svelte";
+  import App from "./App.svelte";
+
+  import Router from "svelte-spa-router";
+  import type { RouteDefinition } from "svelte-spa-router";
+
+  const routes: RouteDefinition = {
+    "/about": About,
+    "/generate": App,
+    "/generate/*": App,
+    "/": App,
+  };
+
+  let prompt: string;
+  let response: string;
+</script>
+
+<style>
+  div.container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    width: 100%;
+    align-items: stretch;
+
+    flex: 1 1 auto;
+  }
+
+  @media (max-width: 900px) {
+    div.container {
+      align-items: center;
+    }
+  }
+</style>
+
+<Header />
+
+<div class="container">
+  <Router {routes} />
+</div>
