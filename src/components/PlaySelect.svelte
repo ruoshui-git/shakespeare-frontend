d480932a550ac156372712ae22742a7b70ad126d
ruoshui-git <ruoshuim@gmail.com>
2020-12-03 19:43:08 -0500

    refactor(PlaySelect) pass id for "for" element

--- src/components/PlaySelect.svelte
+++ src/components/PlaySelect.svelte
@@ -1,12 +1,14 @@
 <!-- Expose id of a selected play -->
 <script lang="ts">
-    import plays from "../play_data";
+  import plays from "../play_data";
 
-    export let playId: number = 6;
+  export let playId: number = 6;
+  let elementId: string | undefined;
+  export { elementId as id };
 </script>
-    
-<select bind:value={playId}>
-    {#each plays as play}
-        <option value={play.id}>{play.name}</option>
-    {/each}
+
+<select id={elementId} bind:value={playId}>
+  {#each plays as play}
+    <option value={play.id}>{play.name}</option>
+  {/each}
 </select>
