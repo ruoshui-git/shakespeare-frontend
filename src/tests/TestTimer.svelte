a59521451e2f485e45dc8c6531575fbc747af628
ruoshui-git <ruoshuim@gmail.com>
2020-12-13 17:12:19 -0500

    chore: test timer store

---
+++ src/tests/TestTimer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import timer from "../common/timer";
+</script>
+
+<p>Time elapsed: {$timer} seconds</p>
+<button on:click={timer.reset}>reset</button>
+<button on:click={timer.start}>start</button>
+<button on:click={timer.stop}>stop</button>
